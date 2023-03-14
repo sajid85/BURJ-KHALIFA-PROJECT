86f36d8a659b61c89e9dabfd2b7f0201c2543cf4
sajid85 <123936901+sajid85@users.noreply.github.com>
2023-03-15 04:48:10 +0600

    Add files via upload

---
+++ mid project/fine_waiver.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+       
+    </head>
+    <body>
+        
+        <form method="post">
+            <fieldset>
+                <legend>Fine waiver</legend>
+            
+            Your request: 
+                    <textarea name="Report" id="Report" >
+                      Apply here for reduction
+                      with valid reason.
+                    </textarea> 
+            
+            
+            <input type="submit" name="" value="Submit">
+            
+        </fieldset>
+        </form>
+    </body>
+</html>
+            <button><a href="homepage.php">BACK </a> </button>
+            
+        </fieldset>
+        </form>
+    </body>
+</html>
---
+++ mid project/login.php
@@ -0,0 +1,27 @@
+<html>
+<form action="login_check.php" method="POST">
+
+<table   width="20%">
+    <tr>
+      <td> <br>
+           <table>
+                <fieldset>
+                    <legend>Login</legend>
+                    <pre>
+                 Name     :<input type="text" name="Name"><br>
+                 Password :<input type="password" name="Password">
+                    </pre>
+                        <hr>
+                    
+                    <input type="submit" name="submit">
+                    
+                    
+                     
+             </fieldset>
+               
+           </table>
+       </td>
+    </tr>
+</table>
+</form>
+</html>
