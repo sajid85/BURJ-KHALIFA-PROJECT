ac4d4344d7042e34e56f0c502355df53006fd742
sajid85 <123936901+sajid85@users.noreply.github.com>
2023-05-20 18:36:47 +0600

    Delete Finalproject directory

--- Finalproject/view/generateUserID.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function generateUserID() {
-    return 'U-' . sprintf('%04d', rand(0, 9999));
-}
-?>
--- Finalproject/view/server.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $data = $_POST['data'];
-    $info = json_decode($data);
- 
-    $data = json_encode($info);
-
-    echo $data;
-?>
