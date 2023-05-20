7f75dda0571810a7f81d13c6415315c2cbaafae9
sajid85 <123936901+sajid85@users.noreply.github.com>
2023-05-20 18:37:45 +0600

    Delete Mid lab exam directory

--- Mid lab exam/logout.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    session_start();
-    unset($_COOKIE['rememberMe']);
-    setcookie('rememberMe',0,time()-3600,'/');
-    $_SESSION=[];
-    header('location: http://localhost/PRD%20management/login.php');
-?>
